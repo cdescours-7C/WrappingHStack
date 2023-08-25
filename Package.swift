a5e615644901134f40fef2beade891b7a4df77c2
cdescours-7C <126065281+cdescours-7C@users.noreply.github.com>
2023-08-25 08:46:47 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "WrappingHStack",
     platforms: [
         .iOS("16.0"),
-        .mac("10.13")
+        .macOS("10.13")
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
